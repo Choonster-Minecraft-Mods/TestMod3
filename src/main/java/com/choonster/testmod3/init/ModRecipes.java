99e5a321230301a81b9fdb55ff3f9ecfc447c2f9
Choonster <Choonster.2010@gmail.com>
2015-08-30 11:38:08 +1000

    Add Shaped Armour Upgrade Recipe
    --
    http://www.minecraftforum.net/forums/mapping-and-modding/minecraft-mods/modification-development/2513998-help-needed-creating-crafting-recipe-with-damaged

--- src/main/java/com/choonster/testmod3/init/ModRecipes.java
+++ src/main/java/com/choonster/testmod3/init/ModRecipes.java
@@ -1,5 +1,6 @@
 package com.choonster.testmod3.init;
 
+import com.choonster.testmod3.recipe.ShapedArmourUpgradeRecipe;
 import com.choonster.testmod3.recipe.ShapelessCuttingRecipe;
 import net.minecraft.block.BlockPlanks;
 import net.minecraft.init.Blocks;
@@ -10,6 +11,7 @@ import net.minecraftforge.fml.common.registry.GameRegistry;
 import net.minecraftforge.oredict.OreDictionary;
 import net.minecraftforge.oredict.RecipeSorter;
 
+import static net.minecraftforge.oredict.RecipeSorter.Category.SHAPED;
 import static net.minecraftforge.oredict.RecipeSorter.Category.SHAPELESS;
 
 public class ModRecipes {
@@ -20,10 +22,14 @@ public class ModRecipes {
 
 	private static void registerRecipeClasses() {
 		RecipeSorter.register("testmod3:shapelesscutting", ShapelessCuttingRecipe.class, SHAPELESS, "after:minecraft:shapeless");
+		RecipeSorter.register("testmod3:shapedarmourupgrade", ShapedArmourUpgradeRecipe.class, SHAPED, "after:forge:shapedore before:minecraft:shapeless");
 	}
 
 	private static void addCraftingRecipes() {
 		GameRegistry.addRecipe(new ShapelessCuttingRecipe(new ItemStack(Blocks.planks, 2, BlockPlanks.EnumType.OAK.getMetadata()), new ItemStack(Items.wooden_axe, 1, OreDictionary.WILDCARD_VALUE), new ItemStack(Blocks.log, 1, BlockPlanks.EnumType.OAK.getMetadata())));
+
+		// Upgrade an Iron Helment to a Golden Helmet while preserving its damage - http://www.minecraftforum.net/forums/mapping-and-modding/minecraft-mods/modification-development/2513998-help-needed-creating-crafting-recipe-with-damaged
+		GameRegistry.addRecipe(new ShapedArmourUpgradeRecipe(Items.golden_helmet, "AAA", "ABA", "AAA", 'A', Blocks.gold_block, 'B', new ItemStack(Items.iron_helmet, 1, OreDictionary.WILDCARD_VALUE)));
 
 		// Recipe for Guardian Spawner - http://www.minecraftforum.net/forums/mapping-and-modding/minecraft-mods/modification-development/2424619-help-needed-creating-non-pig-mob-spawners
 		NBTTagCompound tileEntityData = new NBTTagCompound();
