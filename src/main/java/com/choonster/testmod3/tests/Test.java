b207a59c2d62aff5605fb3500eacb65e1604c687
Choonster <Choonster.2010@gmail.com>
2015-10-30 22:34:28 +1100

    Add NBTTagEnd tests

--- src/main/java/com/choonster/testmod3/tests/Test.java
+++ src/main/java/com/choonster/testmod3/tests/Test.java
@@ -13,14 +13,26 @@ public abstract class Test {
 	protected void assertTrue(boolean value, String message) {
 		if (!value) {
 			allAssertionsPassed = false;
-			logger.warn("Assertion failed: %s", message);
+			logger.warn(String.format("Assertion failed: %s", message));
 		}
 	}
 
 	protected void assertFalse(boolean value, String message) {
 		if (value) {
 			allAssertionsPassed = false;
-			logger.warn("Assertion failed: %s");
+			logger.warn(String.format("Assertion failed: %s", message));
+		}
+	}
+
+	protected void exceptionThrown(Exception e, String message) {
+		allAssertionsPassed = false;
+		logger.warn(message, e);
+	}
+
+	protected void assertEqual(Object expected, Object actual) {
+		if (!expected.equals(actual)) {
+			allAssertionsPassed = false;
+			logger.warn(String.format("Objects not equal. Expected: %s, Actual: %s", expected, actual));
 		}
 	}
 
