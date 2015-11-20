325433a27a7b775f6ec7080ae7c1a5542f4079bf
Choonster <Choonster.2010@gmail.com>
2015-11-21 05:46:00 +1100

    Clearer: Add mode to completely clear chunk

--- src/main/java/com/choonster/testmod3/item/ItemClearer.java
+++ src/main/java/com/choonster/testmod3/item/ItemClearer.java
@@ -11,9 +11,14 @@ import net.minecraft.util.BlockPos;
 import net.minecraft.util.ChatComponentTranslation;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 public class ItemClearer extends Item {
 	private static ImmutableList<Block> whitelist = ImmutableList.of(Blocks.stone, Blocks.dirt, Blocks.grass, Blocks.gravel, Blocks.sand, Blocks.water, Blocks.flowing_water, Blocks.lava, Blocks.flowing_lava, Blocks.ice);
+
+	private static final int MODE_WHITELIST = 0;
+	private static final int MODE_ALL = 1;
 
 	public ItemClearer() {
 		setCreativeTab(TestMod3.creativeTab);
@@ -23,25 +28,42 @@ public class ItemClearer extends Item {
 	@Override
 	public ItemStack onItemRightClick(ItemStack stack, World world, EntityPlayer player) {
 		if (!world.isRemote) {
-			int minX = MathHelper.floor_double(player.posX / 16) * 16;
-			int minZ = MathHelper.floor_double(player.posZ / 16) * 16;
+			int currentMode = stack.getMetadata();
 
-			player.addChatComponentMessage(new ChatComponentTranslation("message.clearer.clearing", minX, minZ));
+			if (player.isSneaking()) {
+				int newMode = currentMode == MODE_ALL ? MODE_WHITELIST : MODE_ALL;
+				stack.setItemDamage(newMode);
+				player.addChatComponentMessage(new ChatComponentTranslation("message.clearer.mode.%s", newMode));
+			} else {
+				int minX = MathHelper.floor_double(player.posX / 16) * 16;
+				int minZ = MathHelper.floor_double(player.posZ / 16) * 16;
 
-			for (int x = minX; x < minX + 16; x++) {
-				for (int z = minZ; z < minZ + 16; z++) {
-					for (int y = 0; y < 256; y++) {
-						BlockPos pos = new BlockPos(x, y, z);
-						if (whitelist.contains(world.getBlockState(pos).getBlock())) {
-							world.setBlockToAir(pos);
+				player.addChatComponentMessage(new ChatComponentTranslation("message.clearer.clearing", minX, minZ));
+
+				for (int x = minX; x < minX + 16; x++) {
+					for (int z = minZ; z < minZ + 16; z++) {
+						for (int y = 0; y < 256; y++) {
+							BlockPos pos = new BlockPos(x, y, z);
+							Block block = world.getBlockState(pos).getBlock();
+							if ((currentMode == MODE_ALL && block != Blocks.bedrock) || whitelist.contains(block)) {
+								world.setBlockState(pos, Blocks.air.getDefaultState(), 2);
+							}
 						}
 					}
 				}
-			}
 
-			player.addChatComponentMessage(new ChatComponentTranslation("message.clearer.cleared"));
+				world.markBlockForUpdate(player.getPosition());
+
+				player.addChatComponentMessage(new ChatComponentTranslation("message.clearer.cleared"));
+			}
 		}
 
 		return super.onItemRightClick(stack, world, player);
+	}
+
+	@SideOnly(Side.CLIENT)
+	@Override
+	public boolean hasEffect(ItemStack stack) {
+		return stack.getMetadata() == MODE_ALL;
 	}
 }
