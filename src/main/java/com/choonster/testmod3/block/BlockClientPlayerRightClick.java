f25c31db6a6fdc8edc66fd21d0edbdf4e9955e61
Choonster TheMage <Choonster.2010@gmail.com>
2016-01-14 15:37:57 +1100

    Extract superclass from BlockClientPlayerClick

--- src/main/java/com/choonster/testmod3/block/BlockClientPlayerRightClick.java
+++ src/main/java/com/choonster/testmod3/block/BlockClientPlayerRightClick.java
@@ -1,69 +1,24 @@
 package com.choonster.testmod3.block;
 
 import com.choonster.testmod3.TestMod3;
-import net.minecraft.block.BlockFence;
 import net.minecraft.block.material.Material;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.Entity;
-import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.BlockPos;
 import net.minecraft.util.ChatComponentText;
-import net.minecraft.world.IBlockAccess;
 import net.minecraft.world.World;
 
 /**
  * A block that forces a player to right click (from the client side) every 10 ticks (0.5 seconds) while standing on it.
- * <p>
- * Uses the pressure plate model, placement and piston movement behaviour; but doesn't depress when stood on.
  * <p>
  * Test for this thread:
  * http://www.minecraftforum.net/forums/mapping-and-modding/minecraft-mods/modification-development/2512904-make-the-player-perform-a-right-click-on-non
  *
  * @author Choonster
  */
-public class BlockClientPlayerRightClick extends BlockTestMod3 {
+public class BlockClientPlayerRightClick extends BlockStaticPressurePlate {
 	public BlockClientPlayerRightClick() {
 		super(Material.rock, "clientPlayerRightClick");
-	}
-
-	@Override
-	public AxisAlignedBB getCollisionBoundingBox(World worldIn, BlockPos pos, IBlockState state) {
-		return null;
-	}
-
-	@Override
-	public boolean isOpaqueCube() {
-		return false;
-	}
-
-	@Override
-	public boolean isFullCube() {
-		return false;
-	}
-
-	@Override
-	public boolean isPassable(IBlockAccess worldIn, BlockPos pos) {
-		return true;
-	}
-
-	@Override
-	public boolean canPlaceBlockAt(World worldIn, BlockPos pos) {
-		return World.doesBlockHaveSolidTopSurface(worldIn, pos) || worldIn.getBlockState(pos).getBlock() instanceof BlockFence;
-	}
-
-	@Override
-	public void setBlockBoundsBasedOnState(IBlockAccess worldIn, BlockPos pos) {
-		setBlockBounds(0.0625F, 0.0F, 0.0625F, 0.9375F, 0.0625F, 0.9375F);
-	}
-
-	@Override
-	public void setBlockBoundsForItemRender() {
-		this.setBlockBounds(0.0F, 0.375F, 0.0F, 1.0F, 0.625F, 1.0F);
-	}
-
-	@Override
-	public int getMobilityFlag() {
-		return 1;
 	}
 
 	@Override
---
+++ src/main/java/com/choonster/testmod3/block/BlockStaticPressurePlate.java
@@ -0,0 +1,76 @@
+package com.choonster.testmod3.block;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockFence;
+import net.minecraft.block.material.Material;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.util.AxisAlignedBB;
+import net.minecraft.util.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraft.world.World;
+
+/**
+ * A block that uses pressure plate model, placement and piston movement behaviour; but doesn't depress when stood on.
+ *
+ * @author Choonster
+ */
+public abstract class BlockStaticPressurePlate extends BlockTestMod3 {
+	public BlockStaticPressurePlate(Material materialIn, String blockName) {
+		super(materialIn, blockName);
+	}
+
+	@Override
+	public AxisAlignedBB getCollisionBoundingBox(World worldIn, BlockPos pos, IBlockState state) {
+		return null;
+	}
+
+	@Override
+	public boolean isOpaqueCube() {
+		return false;
+	}
+
+	@Override
+	public boolean isFullCube() {
+		return false;
+	}
+
+	@Override
+	public boolean isPassable(IBlockAccess worldIn, BlockPos pos) {
+		return true;
+	}
+
+	@Override
+	public boolean canPlaceBlockAt(World worldIn, BlockPos pos) {
+		return canBePlacedOn(worldIn, pos.down());
+	}
+
+	@Override
+	public void onNeighborBlockChange(World worldIn, BlockPos pos, IBlockState state, Block neighborBlock)
+	{
+		if (!this.canBePlacedOn(worldIn, pos.down()))
+		{
+			this.dropBlockAsItem(worldIn, pos, state, 0);
+			worldIn.setBlockToAir(pos);
+		}
+	}
+
+	private boolean canBePlacedOn(World worldIn, BlockPos pos)
+	{
+		return World.doesBlockHaveSolidTopSurface(worldIn, pos) || worldIn.getBlockState(pos).getBlock() instanceof BlockFence;
+	}
+
+	@Override
+	public void setBlockBoundsBasedOnState(IBlockAccess worldIn, BlockPos pos) {
+		setBlockBounds(0.0625F, 0.0F, 0.0625F, 0.9375F, 0.0625F, 0.9375F);
+	}
+
+	@Override
+	public void setBlockBoundsForItemRender() {
+		this.setBlockBounds(0.0F, 0.375F, 0.0F, 1.0F, 0.625F, 1.0F);
+	}
+
+	@Override
+	public int getMobilityFlag() {
+		return 1;
+	}
+}
