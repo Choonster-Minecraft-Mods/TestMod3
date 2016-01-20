a23739c8da5019198f1bb3906fbe3d86c48af55c
Choonster TheMage <Choonster.2010@gmail.com>
2016-01-21 00:04:55 +1100

    Armour Replacement: Create custom ArmorMaterial and textures
    -- Textures are the vanilla Chain textures recoloured red

--- src/main/java/com/choonster/testmod3/init/ModItems.java
+++ src/main/java/com/choonster/testmod3/init/ModItems.java
@@ -2,6 +2,7 @@ package com.choonster.testmod3.init;
 
 import com.choonster.testmod3.item.*;
 import com.choonster.testmod3.util.Constants;
+import net.minecraft.enchantment.Enchantment;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemArmor;
@@ -44,6 +45,7 @@ public class ModItems {
 	public static ItemArmourRestricted replacementBoots;
 
 	public static Item.ToolMaterial TOOL_MATERIAL_GLOWSTONE = EnumHelper.addToolMaterial("glowstone", 1, 5, 0.5f, 1.0f, 10);
+	public static ItemArmor.ArmorMaterial ARMOUR_MATERIAL_REPLACEMENT = EnumHelper.addArmorMaterial(Constants.RESOURCE_PREFIX + "replacement", Constants.RESOURCE_PREFIX + "replacement", 15, new int[]{2, 5, 4, 1}, 12);
 
 	public static void registerItems() {
 		woodenAxe = registerItem(new ItemCuttingAxe(Item.ToolMaterial.WOOD, "woodenAxe"));
@@ -72,15 +74,17 @@ public class ModItems {
 		swapTestA.setOtherItem(new ItemStack(swapTestB));
 		swapTestB.setOtherItem(new ItemStack(swapTestA));
 
-		replacementHelmet = registerItem(new ItemArmourReplacement(ItemArmor.ArmorMaterial.CHAIN, Constants.ARMOUR_TYPE_HEAD, "Replacement"));
-		replacementChestplate = registerItem(new ItemArmourRestricted(ItemArmor.ArmorMaterial.CHAIN, Constants.ARMOUR_TYPE_CHEST, "Replacement"));
-		repacementLeggings = registerItem(new ItemArmourRestricted(ItemArmor.ArmorMaterial.CHAIN, Constants.ARMOUR_TYPE_LEGS, "Replacement"));
-		replacementBoots = registerItem(new ItemArmourRestricted(ItemArmor.ArmorMaterial.CHAIN, Constants.ARMOUR_TYPE_FEET, "Replacement"));
+		replacementHelmet = registerItem(new ItemArmourReplacement(ARMOUR_MATERIAL_REPLACEMENT, Constants.ARMOUR_TYPE_HEAD, "Replacement"));
+		replacementChestplate = registerItem(new ItemArmourRestricted(ARMOUR_MATERIAL_REPLACEMENT, Constants.ARMOUR_TYPE_CHEST, "Replacement"));
+		repacementLeggings = registerItem(new ItemArmourRestricted(ARMOUR_MATERIAL_REPLACEMENT, Constants.ARMOUR_TYPE_LEGS, "Replacement"));
+		replacementBoots = registerItem(new ItemArmourRestricted(ARMOUR_MATERIAL_REPLACEMENT, Constants.ARMOUR_TYPE_FEET, "Replacement"));
+
 		ItemStack chest = new ItemStack(replacementChestplate);
 		chest.addEnchantment(Enchantment.sharpness, 1);
 		replacementHelmet.setReplacementItems(chest, new ItemStack(repacementLeggings), new ItemStack(replacementBoots));
 
 		TOOL_MATERIAL_GLOWSTONE.setRepairItem(new ItemStack(Items.glowstone_dust));
+		ARMOUR_MATERIAL_REPLACEMENT.customCraftingMaterial = modArrow;
 	}
 
 	/**
