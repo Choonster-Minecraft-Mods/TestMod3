8ccc4210fcc2b6396e482e3f3b171842b7612324
Choonster TheMage <Choonster.2010@gmail.com>
2016-03-13 16:43:29 +1100

    CommandRotateVector: Use new VectorUtils methods

--- src/main/java/com/choonster/testmod3/command/CommandRotateVector.java
+++ src/main/java/com/choonster/testmod3/command/CommandRotateVector.java
@@ -1,17 +1,14 @@
 package com.choonster.testmod3.command;
 
+import com.choonster.testmod3.util.VectorUtils;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.CommandException;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.command.WrongUsageException;
 import net.minecraft.util.ChatComponentTranslation;
 import net.minecraft.util.EnumFacing;
-import net.minecraft.util.MathHelper;
-import net.minecraft.util.Vec3i;
 
-import javax.vecmath.AxisAngle4d;
 import javax.vecmath.Matrix3d;
-import javax.vecmath.Matrix4d;
 import javax.vecmath.Vector3d;
 import java.util.Locale;
 
@@ -59,24 +56,17 @@ public class CommandRotateVector extends CommandBase {
 		if (args.length < 5) throw new WrongUsageException("commands.testmod3.rotateVector.usage");
 
 		final double x = parseDouble(args[0]), y = parseDouble(args[1]), z = parseDouble(args[2]);
-		final Vector3d inputVector = new Vector3d(x, y, z);
+		Vector3d inputVector = new Vector3d(x, y, z);
 
 		final EnumFacing.Axis axis = EnumFacing.Axis.byName(args[3].toLowerCase(Locale.ENGLISH));
 		if (axis == null) throw new WrongUsageException("commands.testmod3.rotateVector.invalidAxis");
 
-		// Get the positive facing for the axis then get its direction vector
-		final Vec3i axisVector = EnumFacing.func_181076_a(EnumFacing.AxisDirection.POSITIVE, axis).getDirectionVec();
-
 		final int degrees = parseInt(args[4]);
 
-		final AxisAngle4d axisAngle = new AxisAngle4d(axisVector.getX(), axisVector.getY(), axisVector.getZ(), Math.toRadians(degrees));
+		final Matrix3d rotationMatrix = VectorUtils.getRotationMatrix(axis, Math.toRadians(degrees));
 
-		final Matrix3d rotationMatrix = new Matrix3d();
-		rotationMatrix.set(axisAngle);
+		rotationMatrix.transform(inputVector);
 
-		final Vector3d outputVector = new Vector3d();
-		rotationMatrix.transform(inputVector, outputVector);
-
-		sender.addChatMessage(new ChatComponentTranslation("commands.testmod3.rotateVector.result", outputVector.getX(), outputVector.getY(), outputVector.getZ()));
+		sender.addChatMessage(new ChatComponentTranslation("commands.testmod3.rotateVector.result", inputVector.getX(), inputVector.getY(), inputVector.getZ()));
 	}
 }
