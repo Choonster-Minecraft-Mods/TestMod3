004233120831c3186df7cd7198f47e58a4a521a7
Choonster TheMage <Choonster.2010@gmail.com>
2016-04-04 06:47:31 +1000

    Move IBlockColor/IItemColor registration to dedicated class

--- src/main/java/com/choonster/testmod3/proxy/CombinedClientProxy.java
+++ src/main/java/com/choonster/testmod3/proxy/CombinedClientProxy.java
@@ -3,16 +3,13 @@ package com.choonster.testmod3.proxy;
 
 import com.choonster.testmod3.client.cape.CapeEventHandler;
 import com.choonster.testmod3.client.event.ClientEventHandler;
+import com.choonster.testmod3.client.model.ModColourManager;
 import com.choonster.testmod3.client.model.ModModelManager;
 import com.choonster.testmod3.client.render.entity.RenderModArrow;
 import com.choonster.testmod3.entity.EntityModArrow;
-import com.choonster.testmod3.init.ModBlocks;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.renderer.color.BlockColors;
 import net.minecraft.client.settings.KeyBinding;
 import net.minecraft.entity.player.EntityPlayer;
-import net.minecraft.world.ColorizerGrass;
-import net.minecraft.world.biome.BiomeColorHelper;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.fml.relauncher.Side;
@@ -34,16 +31,7 @@ public class CombinedClientProxy implements IProxy {
 
 	@Override
 	public void init() {
-		final BlockColors blockColors = minecraft.getBlockColors();
-
-		blockColors.registerBlockColorHandler(
-				(state, blockAccess, pos, tintIndex) ->
-						blockAccess != null && pos != null ?
-								BiomeColorHelper.getGrassColorAtPos(blockAccess, pos) :
-								ColorizerGrass.getGrassColor(0.5D, 1.0D),
-				ModBlocks.waterGrass
-		);
-
+		ModColourManager.registerColourHandlers();
 	}
 
 	@Override
