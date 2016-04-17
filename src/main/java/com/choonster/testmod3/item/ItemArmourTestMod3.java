156fb06c694ce9074d502986f6c03c1630af984d
Choonster TheMage <Choonster.2010@gmail.com>
2016-04-18 01:49:06 +1000

    ItemArmourTestMod3: Remove unused field

--- src/main/java/com/choonster/testmod3/item/ItemArmourTestMod3.java
+++ src/main/java/com/choonster/testmod3/item/ItemArmourTestMod3.java
@@ -1,11 +1,8 @@
 package com.choonster.testmod3.item;
 
 import com.choonster.testmod3.TestMod3;
-import com.google.common.collect.ImmutableList;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.ItemArmor;
-
-import java.util.List;
 
 /**
  * Base class for this mod's armour items
@@ -13,10 +10,6 @@ import java.util.List;
  * @author Choonster
  */
 public class ItemArmourTestMod3 extends ItemArmor {
-	/**
-	 * {@link #armorType} to slot name mapping
-	 */
-	private static final List<String> SLOT_NAMES = ImmutableList.of("helmet", "chestplate", "leggings", "boots");
 
 	public ItemArmourTestMod3(final ArmorMaterial material, final EntityEquipmentSlot equipmentSlot, final String armourName) {
 		super(material, -1, equipmentSlot);
