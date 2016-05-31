ce7af7bb93aa74504a1c99f762373ffacd2140df
Choonster TheMage <Choonster.2010@gmail.com>
2016-06-01 06:52:34 +1000

    ItemHandlerNameable: Save custom name in NBT

--- src/main/java/com/choonster/testmod3/inventory/itemhandler/ItemHandlerNameable.java
+++ src/main/java/com/choonster/testmod3/inventory/itemhandler/ItemHandlerNameable.java
@@ -1,6 +1,7 @@
 package com.choonster.testmod3.inventory.itemhandler;
 
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.util.text.ITextComponent;
 import net.minecraftforge.items.ItemStackHandler;
 
@@ -65,5 +66,25 @@ public class ItemHandlerNameable extends ItemStackHandler implements IItemHandle
 	 */
 	public void setDisplayName(ITextComponent displayName) {
 		this.displayName = displayName.createCopy();
+	}
+
+	@Override
+	public NBTTagCompound serializeNBT() {
+		final NBTTagCompound tagCompound = super.serializeNBT();
+
+		if (hasCustomName()) {
+			tagCompound.setString("DisplayName", ITextComponent.Serializer.componentToJson(getDisplayName()));
+		}
+
+		return tagCompound;
+	}
+
+	@Override
+	public void deserializeNBT(NBTTagCompound nbt) {
+		super.deserializeNBT(nbt);
+
+		if (nbt.hasKey("DisplayName")) {
+			setDisplayName(ITextComponent.Serializer.jsonToComponent(nbt.getString("DisplayName")));
+		}
 	}
 }
