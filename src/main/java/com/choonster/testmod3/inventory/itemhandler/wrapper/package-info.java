10e9ccde55744f4621ef84da41d29ac10976fd21
Choonster TheMage <Choonster.2010@gmail.com>
2016-06-01 06:54:25 +1000

    Add missing package-info.java file

---
+++ src/main/java/com/choonster/testmod3/inventory/itemhandler/wrapper/package-info.java
@@ -0,0 +1,7 @@
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+package com.choonster.testmod3.inventory.itemhandler.wrapper;
+
+import mcp.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
