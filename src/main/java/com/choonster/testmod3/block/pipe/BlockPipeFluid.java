baf0d729d58ed797453a689bece2e624e5818624
Choonster TheMage <Choonster.2010@gmail.com>
2016-06-16 00:03:29 +1000

    Fluid Pipe: Update to use new IFluidHandler API

--- src/main/java/com/choonster/testmod3/block/pipe/BlockPipeFluid.java
+++ src/main/java/com/choonster/testmod3/block/pipe/BlockPipeFluid.java
@@ -1,11 +1,15 @@
 package com.choonster.testmod3.block.pipe;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockLiquid;
 import net.minecraft.block.state.IBlockState;
+import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.BlockRenderLayer;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockAccess;
-import net.minecraftforge.fluids.IFluidHandler;
+import net.minecraftforge.fluids.IFluidBlock;
+import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 
@@ -24,7 +28,22 @@ public class BlockPipeFluid extends BlockPipeBasic {
 
 	@Override
 	protected boolean isValidConnection(IBlockState ownState, IBlockState neighbourState, IBlockAccess world, BlockPos ownPos, EnumFacing neighbourDirection) {
-		return super.isValidConnection(ownState, neighbourState, world, ownPos, neighbourDirection) || world.getTileEntity(ownPos.offset(neighbourDirection)) instanceof IFluidHandler;
+		// Connect if the neighbouring block is another pipe
+		if (super.isValidConnection(ownState, neighbourState, world, ownPos, neighbourDirection)) {
+			return true;
+		}
+
+		final BlockPos neighbourPos = ownPos.offset(neighbourDirection);
+		final Block neighbourBlock = neighbourState.getBlock();
+
+		// Connect if the neighbouring block has a TileEntity with an IFluidHandler for the adjacent face
+		if (neighbourBlock.hasTileEntity(neighbourState)) {
+			final TileEntity tileEntity = world.getTileEntity(neighbourPos);
+			return tileEntity != null && tileEntity.hasCapability(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY, neighbourDirection.getOpposite());
+		}
+
+		// Connect if the neighbouring block is a fluid/liquid, FluidUtil.getFluidHandler will provide an IFluidHandler wrapper to drain from it
+		return neighbourBlock instanceof IFluidBlock || neighbourBlock instanceof BlockLiquid;
 	}
 
 	@SideOnly(Side.CLIENT)
