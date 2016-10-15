5079300ad3439c06b4636d7b6f572101a08bcf12
Choonster TheMage <Choonster.2010@gmail.com>
2016-10-15 21:37:09 +1100

    Block Detection Arrow: Add renderer

--- src/main/java/choonster/testmod3/client/renderer/entity/ModRenderers.java
+++ src/main/java/choonster/testmod3/client/renderer/entity/ModRenderers.java
@@ -1,6 +1,7 @@
 package choonster.testmod3.client.renderer.entity;
 
 import choonster.testmod3.TestMod3;
+import choonster.testmod3.entity.EntityBlockDetectionArrow;
 import choonster.testmod3.entity.EntityModArrow;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.util.ResourceLocation;
@@ -14,5 +15,6 @@ import net.minecraftforge.fml.client.registry.RenderingRegistry;
 public class ModRenderers {
 	public static void register() {
 		RenderingRegistry.registerEntityRenderingHandler(EntityModArrow.class, renderManager -> new RenderModArrow(renderManager, new ResourceLocation(TestMod3.MODID, "textures/entity/mod_arrow.png")));
+		RenderingRegistry.registerEntityRenderingHandler(EntityBlockDetectionArrow.class, renderManager -> new RenderModArrow(renderManager, new ResourceLocation(TestMod3.MODID, "textures/entity/block_detection_arrow.png")));
 	}
 }
