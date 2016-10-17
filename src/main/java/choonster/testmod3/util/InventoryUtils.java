90f45c9a2464ce365385f14ebf8599d6981aecc6
Choonster TheMage <Choonster.2010@gmail.com>
2016-10-18 08:10:34 +1100

    Add entity inventory methods for use in Item#onUpdate

--- src/main/java/choonster/testmod3/util/InventoryUtils.java
+++ src/main/java/choonster/testmod3/util/InventoryUtils.java
@@ -2,18 +2,25 @@ package choonster.testmod3.util;
 
 import choonster.testmod3.Logger;
 import com.google.common.base.Throwables;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.inventory.InventoryHelper;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.world.World;
 import net.minecraft.world.storage.loot.LootContext;
 import net.minecraft.world.storage.loot.LootTable;
+import net.minecraftforge.items.CapabilityItemHandler;
 import net.minecraftforge.items.IItemHandler;
+import net.minecraftforge.items.wrapper.PlayerArmorInvWrapper;
+import net.minecraftforge.items.wrapper.PlayerOffhandInvWrapper;
 
+import javax.annotation.Nullable;
 import java.lang.invoke.MethodHandle;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Random;
+import java.util.*;
+import java.util.function.Predicate;
 
 /**
  * Utility methods for inventories.
@@ -124,5 +131,115 @@ public class InventoryUtils {
 		}
 
 		return drops;
+	}
+
+	/**
+	 * An entity inventory type.
+	 */
+	public enum EntityInventoryType {
+		MAIN,
+		HAND,
+		ARMOUR;
+
+		@Override
+		public String toString() {
+			return super.toString().toLowerCase(Locale.ENGLISH);
+		}
+	}
+
+	/**
+	 * Get the main inventory of the specified entity.
+	 * <p>
+	 * For players, this returns the main inventory. For other entities, this returns null.
+	 *
+	 * @param entity The entity
+	 * @return The inventory, if any
+	 */
+	@Nullable
+	public static IItemHandler getMainInventory(Entity entity) {
+		if (entity instanceof EntityPlayer) {
+			return entity.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, EnumFacing.UP);
+		}
+
+		return null;
+	}
+
+	/**
+	 * Get the hand inventory of the specified entity.
+	 * <p>
+	 * For players, this returns the off hand inventory. For other entities, this returns the {@link EnumFacing#UP} {@link IItemHandler} capability.
+	 *
+	 * @param entity The entity
+	 * @return The hand inventory, if any
+	 */
+	@Nullable
+	public static IItemHandler getHandInventory(Entity entity) {
+		if (entity instanceof EntityPlayer) {
+			return new PlayerOffhandInvWrapper(((EntityPlayer) entity).inventory);
+		}
+
+		return entity.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, EnumFacing.UP);
+	}
+
+	/**
+	 * Get the armour inventory of the specified entity.
+	 * <p>
+	 * For players, this returns the armour inventory. For other entities, this returns the {@link EnumFacing#NORTH} {@link IItemHandler} capability.
+	 *
+	 * @param entity The entity
+	 * @return The inventory, if any
+	 */
+	@Nullable
+	public static IItemHandler getArmourInventory(Entity entity) {
+		if (entity instanceof EntityPlayer) {
+			return new PlayerArmorInvWrapper(((EntityPlayer) entity).inventory);
+		}
+
+		return entity.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, EnumFacing.NORTH);
+	}
+
+	/**
+	 * Get the entity's inventory of the specified type.
+	 *
+	 * @param entity        The entity
+	 * @param inventoryType The inventory type.
+	 * @return The inventory, if any
+	 */
+	@Nullable
+	public static IItemHandler getInventoryForType(final Entity entity, final EntityInventoryType inventoryType) {
+		switch (inventoryType) {
+			case MAIN:
+				return getMainInventory(entity);
+			case HAND:
+				return getHandInventory(entity);
+			case ARMOUR:
+				return getArmourInventory(entity);
+		}
+
+		return null;
+	}
+
+	/**
+	 * Try to perform an operation for each of the specified inventory types, stopping at the first successful operation.
+	 * <p>
+	 * Only performs the operation on inventory types that exist for the entity.
+	 * <p>
+	 * This is mainly useful in {@link Item#onUpdate(ItemStack, World, Entity, int, boolean)}, where the item can be in any of the player's inventories.
+	 *
+	 * @param entity         The entity
+	 * @param operation      The operation to perform
+	 * @param inventoryTypes The inventory types to perform the operation on, in order
+	 * @return The inventory type of the first successful operation, or null if all operations failed
+	 */
+	@Nullable
+	public static EntityInventoryType forEachEntityInventory(final Entity entity, final Predicate<IItemHandler> operation, final EntityInventoryType... inventoryTypes) {
+		for (final EntityInventoryType inventoryType : inventoryTypes) {
+			final IItemHandler inventory = getInventoryForType(entity, inventoryType);
+			if (inventory != null && operation.test(inventory)) {
+				return inventoryType;
+			}
+		}
+
+		return null;
 	}
 }
