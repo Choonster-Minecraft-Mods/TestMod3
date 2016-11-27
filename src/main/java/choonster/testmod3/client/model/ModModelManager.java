7b97c850bc7202f1b4079f76c10aade5a948f649
Choonster TheMage <Choonster.2010@gmail.com>
2016-11-27 17:35:05 +1100

    Overhaul model registration
    - Use StateMapperBase#getPropertyString to generate property strings instead of using String.format
    - Add doc comments
    - Remove unused methods

--- src/main/java/choonster/testmod3/client/model/ModModelManager.java
+++ src/main/java/choonster/testmod3/client/model/ModModelManager.java
@@ -1,15 +1,17 @@
 package choonster.testmod3.client.model;
 
-import choonster.testmod3.block.BlockColouredSlab;
-import choonster.testmod3.block.BlockVariants;
+import choonster.testmod3.block.*;
 import choonster.testmod3.init.ModBlocks;
 import choonster.testmod3.init.ModFluids;
 import choonster.testmod3.init.ModItems;
 import choonster.testmod3.item.ItemVariants;
 import choonster.testmod3.util.Constants;
+import choonster.testmod3.util.EnumFaceRotation;
 import choonster.testmod3.util.IVariant;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockLiquid;
+import net.minecraft.block.BlockSlab;
+import net.minecraft.block.properties.IProperty;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.renderer.ItemMeshDefinition;
 import net.minecraft.client.renderer.block.model.ModelBakery;
@@ -19,8 +21,11 @@ import net.minecraft.client.renderer.block.statemap.StateMapperBase;
 import net.minecraft.init.Items;
 import net.minecraft.item.EnumDyeColor;
 import net.minecraft.item.Item;
+import net.minecraft.util.EnumFacing;
+import net.minecraft.util.IStringSerializable;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.client.model.ModelLoader;
+import net.minecraftforge.fluids.Fluid;
 import net.minecraftforge.fluids.IFluidBlock;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -28,6 +33,7 @@ import net.minecraftforge.fml.relauncher.Side;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.function.ToIntFunction;
 
 @Mod.EventBusSubscriber(Side.CLIENT)
 public class ModModelManager {
@@ -39,7 +45,7 @@ public class ModModelManager {
 	}
 
 	/**
-	 * Register this mod's fluid, block and item models.
+	 * Register this mod's {@link Fluid}, {@link Block} and {@link Item} models.
 	 *
 	 * @param event The event
 	 */
@@ -50,10 +56,18 @@ public class ModModelManager {
 		INSTANCE.registerItemModels();
 	}
 
+	/**
+	 * Register this mod's {@link Fluid} models.
+	 */
 	private void registerFluidModels() {
 		ModFluids.MOD_FLUID_BLOCKS.forEach(this::registerFluidModel);
 	}
 
+	/**
+	 * Register the block and item model for a {@link Fluid}.
+	 *
+	 * @param fluidBlock The Fluid's Block
+	 */
 	private void registerFluidModel(IFluidBlock fluidBlock) {
 		final Item item = Item.getItemFromBlock((Block) fluidBlock);
 		assert item != Items.AIR;
@@ -72,66 +86,168 @@ public class ModModelManager {
 		});
 	}
 
+	/**
+	 * A {@link StateMapperBase} used to create property strings.
+	 */
+	private final StateMapperBase propertyStringMapper = new StateMapperBase() {
+		@Override
+		protected ModelResourceLocation getModelResourceLocation(IBlockState state) {
+			return new ModelResourceLocation("minecraft:air");
+		}
+	};
+
+	/**
+	 * Register this mod's {@link Block} models.
+	 */
 	private void registerBlockModels() {
 		ModelLoader.setCustomStateMapper(ModBlocks.WATER_GRASS, new StateMap.Builder().ignore(BlockLiquid.LEVEL).build());
 
-		registerBlockItemModel(ModBlocks.RIGHT_CLICK_TEST, new ModelResourceLocation(ModBlocks.RIGHT_CLICK_TEST.getRegistryName(), "has_ender_eye=false"));
+		registerBlockItemModel(
+				ModBlocks.RIGHT_CLICK_TEST.getDefaultState()
+						.withProperty(BlockRightClickTest.HAS_ENDER_EYE, false)
+		);
 
-		for (final EnumDyeColor color : EnumDyeColor.values()) {
-			registerBlockItemModelForMeta(ModBlocks.COLORED_ROTATABLE, color.getMetadata(), String.format("color=%s,facing=north", color.getName()));
-			registerBlockItemModelForMeta(ModBlocks.COLORED_MULTI_ROTATABLE, color.getMetadata(), String.format("color=%s,face_rotation=up,facing=north", color.getName()));
+		registerVariantBlockItemModels(
+				ModBlocks.COLORED_ROTATABLE.getDefaultState()
+						.withProperty(BlockColoredRotatable.FACING, EnumFacing.NORTH),
+				BlockColoredRotatable.COLOR, EnumDyeColor::getMetadata
+		);
 
-			final BlockColouredSlab.EnumColourGroup colourGroup = BlockColouredSlab.EnumColourGroup.getGroupForColour(color);
-			if (colourGroup != null) {
-				registerItemModelForMeta(ModBlocks.Slabs.STAINED_CLAY_SLABS.getSlabGroupByColourGroup(colourGroup).item, colourGroup.getOffsetMetadata(color), String.format("colour=%s,half=bottom", color.getName()));
-			}
-		}
+		registerVariantBlockItemModels(
+				ModBlocks.COLORED_MULTI_ROTATABLE.getDefaultState()
+						.withProperty(BlockColoredMultiRotatable.FACING, EnumFacing.NORTH)
+						.withProperty(BlockColoredMultiRotatable.FACE_ROTATION, EnumFaceRotation.UP),
+				BlockColoredMultiRotatable.COLOR, EnumDyeColor::getMetadata
+		);
 
-		registerVariantBlockItemModels(ModBlocks.VARIANTS, "variant", BlockVariants.EnumType.values());
+		registerSlabGroupItemModels(ModBlocks.Slabs.STAINED_CLAY_SLABS.high);
+		registerSlabGroupItemModels(ModBlocks.Slabs.STAINED_CLAY_SLABS.low);
 
-		registerBlockItemModel(ModBlocks.MIRROR_PLANE, new ModelResourceLocation(ModBlocks.MIRROR_PLANE.getRegistryName(), "horizontal_rotation=north,vertical_rotation=up"));
-		registerBlockItemModel(ModBlocks.CHEST, new ModelResourceLocation(ModBlocks.CHEST.getRegistryName(), "facing=north"));
+		registerVariantBlockItemModels(
+				ModBlocks.VARIANTS.getDefaultState(),
+				BlockVariants.VARIANT
+		);
+
+		registerBlockItemModel(
+				ModBlocks.MIRROR_PLANE.getDefaultState()
+						.withProperty(BlockPlane.HORIZONTAL_ROTATION, EnumFacing.NORTH)
+						.withProperty(BlockPlane.VERTICAL_ROTATION, BlockPlane.EnumVerticalRotation.UP)
+		);
+
+		registerBlockItemModel(
+				ModBlocks.CHEST.getDefaultState()
+						.withProperty(BlockModChest.FACING, EnumFacing.NORTH)
+		);
+
 
 		ModBlocks.RegistrationHandler.ITEM_BLOCKS.stream().filter(item -> !itemsRegistered.contains(item)).forEach(this::registerItemModel);
 	}
 
-	private void registerBlockItemModel(Block block) {
+	/**
+	 * Register a single model for the {@link Block}'s {@link Item}.
+	 * <p>
+	 * Uses the registry name as the domain/path and the {@link IBlockState} as the variant.
+	 *
+	 * @param state The state to use as the variant
+	 */
+	private void registerBlockItemModel(IBlockState state) {
+		final Block block = state.getBlock();
 		final Item item = Item.getItemFromBlock(block);
+
 		if (item != Items.AIR) {
-			registerItemModel(item);
+			registerItemModel(item, new ModelResourceLocation(block.getRegistryName(), propertyStringMapper.getPropertyString(state.getProperties())));
 		}
 	}
 
-	private void registerBlockItemModel(Block block, String modelLocation) {
-		final Item item = Item.getItemFromBlock(block);
+	/**
+	 * Register a model for a metadata value of the {@link Block}'s {@link Item}.
+	 * <p>
+	 * Uses the registry name as the domain/path and the {@link IBlockState} as the variant.
+	 *
+	 * @param state    The state to use as the variant
+	 * @param metadata The item metadata to register the model for
+	 */
+	private void registerBlockItemModelForMeta(IBlockState state, int metadata) {
+		final Item item = Item.getItemFromBlock(state.getBlock());
+
 		if (item != Items.AIR) {
-			registerItemModel(item, modelLocation);
+			registerItemModelForMeta(item, metadata, propertyStringMapper.getPropertyString(state.getProperties()));
 		}
 	}
 
-	private <T extends IVariant> void registerVariantBlockItemModels(Block block, String variantName, T[] variants) {
-		final Item item = Item.getItemFromBlock(block);
-		if (item != Items.AIR) {
-			registerVariantItemModels(item, variantName, variants);
-		}
+	/**
+	 * Register a model for each metadata value of the {@link Block}'s {@link Item} corresponding to the values of an {@link IProperty}.
+	 * <p>
+	 * For each value:
+	 * <li>The domain/path is the registry name</li>
+	 * <li>The variant is {@code baseState} with the {@link IProperty} set to the value</li>
+	 * <p>
+	 * The {@code getMeta} function is used to get the metadata of each value.
+	 *
+	 * @param baseState The base state to use for the variant
+	 * @param property  The property whose values should be used
+	 * @param getMeta   A function to get the metadata of each value
+	 * @param <T>       The value type
+	 */
+	private <T extends Comparable<T>> void registerVariantBlockItemModels(IBlockState baseState, IProperty<T> property, ToIntFunction<T> getMeta) {
+		property.getAllowedValues().forEach(value -> registerBlockItemModelForMeta(baseState.withProperty(property, value), getMeta.applyAsInt(value)));
 	}
 
-	private void registerBlockItemModel(Block block, ModelResourceLocation fullModelLocation) {
-		final Item item = Item.getItemFromBlock(block);
-		if (item != Items.AIR) {
-			registerItemModel(item, fullModelLocation);
-		}
+	/**
+	 * Register a model for each metadata value of the {@link Block}'s {@link Item} corresponding to the values of an {@link IProperty}.
+	 * <p>
+	 * For each value:
+	 * <li>The domain/path is the registry name</li>
+	 * <li>The variant is {@code baseState} with the {@link IProperty} set to the value</li>
+	 * <p>
+	 * {@link IVariant#getMeta()} is used to get the metadata of each value.
+	 *
+	 * @param baseState The base state to use for the variant
+	 * @param property  The property whose values should be used
+	 * @param <T>       The value type
+	 */
+	private <T extends IVariant & Comparable<T>> void registerVariantBlockItemModels(IBlockState baseState, IProperty<T> property) {
+		registerVariantBlockItemModels(baseState, property, IVariant::getMeta);
 	}
 
-	private void registerBlockItemModelForMeta(Block block, int metadata, String variant) {
-		final Item item = Item.getItemFromBlock(block);
-		if (item != Items.AIR) {
-			registerItemModelForMeta(item, metadata, variant);
-		}
+	/**
+	 * Register a model for each metadata value of a {@link BlockSlabTestMod3.SlabGroup}'s {@link Item} corresponding to
+	 * the values of the slab's variant property ({@link BlockSlab#getVariantProperty()}).
+	 * <p>
+	 * For each value:
+	 * <li>The domain/path is the registry name</li>
+	 * <li>The variant is the default state with {@link BlockSlab#HALF} set to {@link BlockSlab.EnumBlockHalf#BOTTOM}
+	 * and the variant property set to the value as the variant.</li>
+	 * <p>
+	 * {@link BlockSlabTestMod3#getMetadata} is used to get the metadata of each value.
+	 *
+	 * @param slabGroup  The SlabGroup
+	 * @param <VARIANT>  The variant type
+	 * @param <VARIANTS> The variant collection type
+	 * @param <SLAB>     The slab type
+	 */
+	private <
+			VARIANT extends Enum<VARIANT> & IStringSerializable,
+			VARIANTS extends Iterable<VARIANT> & IStringSerializable,
+			SLAB extends BlockSlabTestMod3<VARIANT, VARIANTS, SLAB>
+			>
+	void registerSlabGroupItemModels(BlockSlabTestMod3.SlabGroup<VARIANT, VARIANTS, SLAB> slabGroup) {
+		final SLAB singleSlab = slabGroup.singleSlab;
+		registerVariantBlockItemModels(
+				singleSlab.getDefaultState()
+						.withProperty(BlockSlab.HALF, BlockSlab.EnumBlockHalf.BOTTOM),
+				singleSlab.getVariantProperty(), singleSlab::getMetadata
+		);
 	}
 
+	/**
+	 * The {@link Item}s that have had models registered so far.
+	 */
 	private final Set<Item> itemsRegistered = new HashSet<>();
 
+	/**
+	 * Register this mod's {@link Item} models.
+	 */
 	private void registerItemModels() {
 		// Register items with custom model names first
 		registerItemModel(ModItems.SNOWBALL_LAUNCHER, "minecraft:fishing_rod");
@@ -165,35 +281,94 @@ public class ModModelManager {
 		ModItems.RegistrationHandler.ITEMS.stream().filter(item -> !itemsRegistered.contains(item)).forEach(this::registerItemModel);
 	}
 
+	/**
+	 * Register a single model for an {@link Item}.
+	 * <p>
+	 * Uses the registry name as the domain/path and {@code "inventory"} as the variant.
+	 *
+	 * @param item The Item
+	 */
 	private void registerItemModel(Item item) {
 		registerItemModel(item, item.getRegistryName().toString());
 	}
 
+	/**
+	 * Register a single model for an {@link Item}.
+	 * <p>
+	 * Uses {@code modelLocation} as the domain/path and {@link "inventory"} as the variant.
+	 *
+	 * @param item          The Item
+	 * @param modelLocation The model location
+	 */
 	private void registerItemModel(Item item, String modelLocation) {
 		final ModelResourceLocation fullModelLocation = new ModelResourceLocation(modelLocation, "inventory");
 		registerItemModel(item, fullModelLocation);
 	}
 
+	/**
+	 * Register a single model for an {@link Item}.
+	 * <p>
+	 * Uses {@code fullModelLocation} as the domain, path and variant.
+	 *
+	 * @param item              The Item
+	 * @param fullModelLocation The full model location
+	 */
 	private void registerItemModel(Item item, ModelResourceLocation fullModelLocation) {
 		ModelBakery.registerItemVariants(item, fullModelLocation); // Ensure the custom model is loaded and prevent the default model from being loaded
 		registerItemModel(item, MeshDefinitionFix.create(stack -> fullModelLocation));
 	}
 
+	/**
+	 * Register an {@link ItemMeshDefinition} for an {@link Item}.
+	 *
+	 * @param item           The Item
+	 * @param meshDefinition The ItemMeshDefinition
+	 */
 	private void registerItemModel(Item item, ItemMeshDefinition meshDefinition) {
 		itemsRegistered.add(item);
 		ModelLoader.setCustomMeshDefinition(item, meshDefinition);
 	}
 
-	private <T extends IVariant> void registerVariantItemModels(Item item, String variantName, T[] variants) {
-		for (T variant : variants) {
-			registerItemModelForMeta(item, variant.getMeta(), variantName + "=" + variant.getName());
+	/**
+	 * Register a model for each metadata value of an {@link Item} corresponding to the values in {@code values}.
+	 * <p>
+	 * Uses the registry name as the domain/path and {@code "[variantName]=[valueName]"} as the variant.
+	 * <p>
+	 * Uses {@link IVariant#getMeta()} to determine the metadata of each value.
+	 *
+	 * @param item        The Item
+	 * @param variantName The variant name
+	 * @param values      The values
+	 * @param <T>         The value type
+	 */
+	private <T extends IVariant> void registerVariantItemModels(Item item, String variantName, T[] values) {
+		for (T value : values) {
+			registerItemModelForMeta(item, value.getMeta(), variantName + "=" + value.getName());
 		}
 	}
 
+	/**
+	 * Register a model for a metadata value an {@link Item}.
+	 * <p>
+	 * Uses the registry name as the domain/path and {@code variant} as the variant.
+	 *
+	 * @param item     The Item
+	 * @param metadata The metadata
+	 * @param variant  The variant
+	 */
 	private void registerItemModelForMeta(Item item, int metadata, String variant) {
 		registerItemModelForMeta(item, metadata, new ModelResourceLocation(item.getRegistryName(), variant));
 	}
 
+	/**
+	 * Register a model for a metadata value of an {@link Item}.
+	 * <p>
+	 * Uses {@code modelResourceLocation} as the domain, path and variant.
+	 *
+	 * @param item                  The Item
+	 * @param metadata              The metadata
+	 * @param modelResourceLocation The full model location
+	 */
 	private void registerItemModelForMeta(Item item, int metadata, ModelResourceLocation modelResourceLocation) {
 		itemsRegistered.add(item);
 		ModelLoader.setCustomModelResourceLocation(item, metadata, modelResourceLocation);
