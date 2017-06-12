0baf903d18e8fe6a70868b9b15cefbdba03e2026
Choonster TheMage <Choonster.2010@gmail.com>
2017-06-13 04:05:34 +1000

    Update MCP mappings

--- src/main/java/choonster/testmod3/block/BlockStaticPressurePlate.java
+++ src/main/java/choonster/testmod3/block/BlockStaticPressurePlate.java
@@ -44,7 +44,7 @@ public abstract class BlockStaticPressurePlate extends BlockTestMod3 {
 	}
 
 	@Override
-	public boolean blocksMovement(final IBlockAccess worldIn, final BlockPos pos) {
+	public boolean isPassable(final IBlockAccess worldIn, final BlockPos pos) {
 		return true;
 	}
 
