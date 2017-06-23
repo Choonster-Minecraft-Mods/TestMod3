a14d74f6f1562e2cabdc687640bb43a9c192df28
Choonster TheMage <Choonster.2010@gmail.com>
2017-06-24 05:36:10 +1000

    Remove unnecessary @Mod.EventBusSubscriber annotation

--- src/main/java/choonster/testmod3/capability/CapabilityContainerListenerManager.java
+++ src/main/java/choonster/testmod3/capability/CapabilityContainerListenerManager.java
@@ -19,7 +19,6 @@ import java.util.function.Function;
  *
  * @author Choonster
  */
-@Mod.EventBusSubscriber
 public class CapabilityContainerListenerManager {
 	/**
 	 * The {@link CapabilityContainerListener} factories.
