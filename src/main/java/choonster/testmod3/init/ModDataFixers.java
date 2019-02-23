81ed3bea795a95af7581b391206bc1c297d8d49b
Choonster TheMage <Choonster.2010@gmail.com>
2019-02-23 14:09:22 +1100

    Add flattening for item forms of flattened blocks

--- src/main/java/choonster/testmod3/init/ModDataFixers.java
+++ src/main/java/choonster/testmod3/init/ModDataFixers.java
@@ -2,6 +2,7 @@ package choonster.testmod3.init;
 
 import choonster.testmod3.TestMod3;
 import choonster.testmod3.datafix.BlockFlatteningDefinitions;
+import choonster.testmod3.datafix.ItemFlatteningDefinitions;
 import net.minecraft.util.datafix.FixTypes;
 import net.minecraftforge.common.util.ModFixs;
 import net.minecraftforge.fml.common.FMLCommonHandler;
@@ -15,7 +16,7 @@ public class ModDataFixers {
 	/**
 	 * The current data version.
 	 */
-	private static final int DATA_VERSION = 102;
+	private static final int DATA_VERSION = 103;
 
 	/**
 	 * Register this mod's data fixers.
@@ -24,5 +25,6 @@ public class ModDataFixers {
 		final ModFixs modFixs = FMLCommonHandler.instance().getDataFixer().init(TestMod3.MODID, DATA_VERSION);
 
 		modFixs.registerFix(FixTypes.CHUNK, BlockFlatteningDefinitions.createBlockFlattening());
+		modFixs.registerFix(FixTypes.ITEM_INSTANCE, ItemFlatteningDefinitions.createItemFlattening());
 	}
 }
