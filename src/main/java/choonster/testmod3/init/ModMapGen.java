8020202f6ed3b71d6d9efe2373fe6ce5959d6d4d
Choonster TheMage <Choonster.2010@gmail.com>
2019-03-01 23:02:11 +1100

    Remove MapGenScatteredFeaturesModBiomes
    -- Biomes can easily add the scattered feature structures to themselves now

--- src/main/java/choonster/testmod3/init/ModMapGen.java
+++ src/main/java/choonster/testmod3/init/ModMapGen.java
@@ -1,20 +1,10 @@
 package choonster.testmod3.init;
 
-import choonster.testmod3.event.MapGenHandler;
 import choonster.testmod3.world.gen.WorldGenBanner;
 import choonster.testmod3.world.gen.WorldGenOres;
-import choonster.testmod3.world.gen.structure.MapGenScatteredFeatureModBiomes;
-import net.minecraft.world.gen.structure.MapGenStructureIO;
-import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.registry.GameRegistry;
 
 public class ModMapGen {
-	public static void registerMapGen() {
-		MapGenStructureIO.registerStructure(MapGenScatteredFeatureModBiomes.Start.class, "testmod3:MapGenScatteredFeatureModBiomes");
-
-		MinecraftForge.TERRAIN_GEN_BUS.register(new MapGenHandler());
-	}
-
 	public static void registerWorldGenerators() {
 		GameRegistry.registerWorldGenerator(new WorldGenBanner(), 100);
 		GameRegistry.registerWorldGenerator(new WorldGenOres(), 0);
--- src/main/java/choonster/testmod3/world/gen/structure/package-info.java
+++
@@ -1,7 +0,0 @@
-@ParametersAreNonnullByDefault
-@MethodsReturnNonnullByDefault
-package choonster.testmod3.world.gen.structure;
-
-import mcp.MethodsReturnNonnullByDefault;
-
-import javax.annotation.ParametersAreNonnullByDefault;
