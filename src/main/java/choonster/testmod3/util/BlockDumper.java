9a2291f96935dcd7d3b4a9a5b7ae1bf023ed8d79
Choonster TheMage <Choonster.2010@gmail.com>
2019-03-10 03:06:49 +1100

    Update BlockDumper

--- src/main/java/choonster/testmod3/util/BlockDumper.java
+++ src/main/java/choonster/testmod3/util/BlockDumper.java
@@ -4,8 +4,8 @@ import choonster.testmod3.TestMod3;
 import com.google.common.base.Charsets;
 import net.minecraft.init.Items;
 import net.minecraft.item.Item;
-import net.minecraftforge.fml.common.FMLCommonHandler;
-import net.minecraftforge.fml.common.registry.ForgeRegistries;
+import net.minecraftforge.fml.loading.FMLEnvironment;
+import net.minecraftforge.registries.ForgeRegistries;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -16,7 +16,7 @@ import java.util.stream.StreamSupport;
 /**
  * Dumps the translation keys and the output of the {@link net.minecraft.item.ItemBlock}'s {@link Object#toString()} method for all of this mod's blocks.
  * <p>
- * I wrote this because I was getting an {@link AbstractMethodError} from a lambda implementing {@link net.minecraft.client.renderer.ItemMeshDefinition} and the only the toString output was included in the crash report.
+ * I wrote this because I was getting an {@link AbstractMethodError} from a lambda implementing ItemMeshDefinition and the only the toString output was included in the crash report.
  *
  * @author Choonster
  */
@@ -24,13 +24,13 @@ public class BlockDumper {
 	private static final Logger LOGGER = LogManager.getLogger();
 
 	public static void dump() {
-		try (final PrintWriter writer = new PrintWriter("TestMod3_BlockDump_" + (FMLCommonHandler.instance().getSide().isClient() ? "Client" : "Server") + ".txt", Charsets.UTF_8.toString())) {
+		try (final PrintWriter writer = new PrintWriter("TestMod3_BlockDump_" + (FMLEnvironment.dist.isClient() ? "Client" : "Server") + ".txt", Charsets.UTF_8.toString())) {
 			writer.println("Name - toString");
 
 			StreamSupport.stream(ForgeRegistries.BLOCKS.spliterator(), false)
 					.filter(block -> Objects.requireNonNull(block.getRegistryName()).getNamespace().equals(TestMod3.MODID))
 					.forEach(block -> {
-						final Item item = Item.getItemFromBlock(block);
+						final Item item = block.asItem();
 						if (item != Items.AIR) {
 							writer.printf("%s - %s\n", item.getTranslationKey(), item.toString());
 						}
