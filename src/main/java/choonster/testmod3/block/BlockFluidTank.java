740b921b086612bf72494d35832b285952a655b6
Choonster TheMage <Choonster.2010@gmail.com>
2019-03-10 18:59:00 +1100

    Update BlockFluidTank

--- src/main/java/choonster/testmod3/block/BlockFluidTank.java
+++ src/main/java/choonster/testmod3/block/BlockFluidTank.java
@@ -3,7 +3,7 @@ package choonster.testmod3.block;
 import choonster.testmod3.TestMod3;
 import choonster.testmod3.network.MessageFluidTankContents;
 import choonster.testmod3.tileentity.TileEntityFluidTank;
-import choonster.testmod3.util.CapabilityUtils;
+import choonster.testmod3.tileentity.TileEntityFluidTankBase;
 import net.minecraft.block.Block;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.entity.EntityLivingBase;
@@ -22,12 +22,14 @@ import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
+import net.minecraftforge.common.util.LazyOptional;
 import net.minecraftforge.fluids.FluidActionResult;
 import net.minecraftforge.fluids.FluidStack;
 import net.minecraftforge.fluids.FluidUtil;
 import net.minecraftforge.fluids.capability.CapabilityFluidHandler;
 import net.minecraftforge.fluids.capability.IFluidHandler;
 import net.minecraftforge.fluids.capability.IFluidTankProperties;
+import net.minecraftforge.fml.network.PacketDistributor;
 
 import javax.annotation.Nullable;
 import java.util.ArrayList;
@@ -38,22 +40,20 @@ import java.util.List;
  *
  * @author Choonster
  */
-public class BlockFluidTank<TE extends TileEntityFluidTank> extends BlockTileEntity<TE> {
+public class BlockFluidTank<TE extends TileEntityFluidTankBase> extends BlockTileEntity<TE> {
 	public BlockFluidTank(final Block.Properties properties) {
 		super(true, properties);
 	}
 
 	@Override
 	public void getDrops(final IBlockState state, final NonNullList<ItemStack> drops, final World world, final BlockPos pos, final int fortune) {
-		final IFluidHandler fluidHandler = getFluidHandler(world, pos);
-
-		if (fluidHandler != null) {
+		getFluidHandler(world, pos).ifPresent(fluidHandler -> {
 			final FluidActionResult fluidActionResult = FluidUtil.tryFillContainer(new ItemStack(this), fluidHandler, Integer.MAX_VALUE, null, true);
 
 			if (fluidActionResult.isSuccess()) {
 				drops.add(fluidActionResult.getResult());
 			}
-		}
+		});
 	}
 
 	/**
@@ -61,22 +61,19 @@ public class BlockFluidTank<TE extends TileEntityFluidTank> extends BlockTileEnt
 	 *
 	 * @param world The world
 	 * @param pos   The position
-	 * @return The IFluidHandler
+	 * @return A lazy optional containing the IFluidHandler, if it exists
 	 */
-	@Nullable
-	private IFluidHandler getFluidHandler(final IBlockReader world, final BlockPos pos) {
-		return CapabilityUtils.getCapability(getTileEntity(world, pos), CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY, null);
+	private LazyOptional<IFluidHandler> getFluidHandler(final IBlockReader world, final BlockPos pos) {
+		return getTileEntity(world, pos).getCapability(CapabilityFluidHandler.FLUID_HANDLER_CAPABILITY, null);
 	}
 
 	@Override
-	public void onBlockPlacedBy(final World worldIn, final BlockPos pos, final IBlockState state, final EntityLivingBase placer, final ItemStack stack) {
-		final IFluidHandler fluidHandler = getFluidHandler(worldIn, pos);
-		if (fluidHandler != null) {
+	public void onBlockPlacedBy(final World worldIn, final BlockPos pos, final IBlockState state, @Nullable final EntityLivingBase placer, final ItemStack stack) {
+		getFluidHandler(worldIn, pos).ifPresent(fluidHandler -> {
 			FluidUtil.tryEmptyContainer(stack, fluidHandler, Integer.MAX_VALUE, null, true);
-		}
+		});
 	}
 
-	@Nullable
 	@Override
 	public TileEntity createTileEntity(final IBlockState state, final IBlockReader world) {
 		return new TileEntityFluidTank();
@@ -107,22 +104,20 @@ public class BlockFluidTank<TE extends TileEntityFluidTank> extends BlockTileEnt
 	@Override
 	public boolean onBlockActivated(final IBlockState state, final World worldIn, final BlockPos pos, final EntityPlayer player, final EnumHand hand, final EnumFacing side, final float hitX, final float hitY, final float hitZ) {
 		final ItemStack heldItem = player.getHeldItem(hand);
-		final IFluidHandler fluidHandler = getFluidHandler(worldIn, pos);
+		return getFluidHandler(worldIn, pos)
+				.map(fluidHandler -> {
+					// Try fill/empty the held fluid container from the tank
+					final boolean success = FluidUtil.interactWithFluidHandler(player, hand, worldIn, pos, side);
 
-		if (fluidHandler != null) {
-			// Try fill/empty the held fluid container from the tank
-			final boolean success = FluidUtil.interactWithFluidHandler(player, hand, worldIn, pos, side);
+					// If the contents changed or this is the off hand, send a chat message to the player
+					if (!worldIn.isRemote && (success || hand == EnumHand.OFF_HAND)) {
+						TestMod3.network.send(PacketDistributor.PLAYER.with(() -> (EntityPlayerMP) player), new MessageFluidTankContents(fluidHandler.getTankProperties()));
+					}
 
-			// If the contents changed or this is the off hand, send a chat message to the player
-			if (!worldIn.isRemote && (success || hand == EnumHand.OFF_HAND)) {
-				TestMod3.network.sendTo(new MessageFluidTankContents(fluidHandler.getTankProperties()), (EntityPlayerMP) player);
-			}
-
-			// If the held item is a fluid container, stop processing here so it doesn't try to place its contents
-			return heldItem.getCapability(CapabilityFluidHandler.FLUID_HANDLER_ITEM_CAPABILITY).isPresent();
-		}
-
-		return false;
+					// If the held item is a fluid container, stop processing here so it doesn't try to place its contents
+					return heldItem.getCapability(CapabilityFluidHandler.FLUID_HANDLER_ITEM_CAPABILITY).isPresent();
+				})
+				.orElse(false);
 	}
 
 	@SuppressWarnings("deprecation")
