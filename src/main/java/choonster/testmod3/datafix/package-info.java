77d5d1dbb166e28777d00976c3610cc94734c78c
Choonster TheMage <Choonster.2010@gmail.com>
2019-06-08 19:10:41 +1000

    Comment out ModDataFixers class and delete flattening data fixers
    -- The new data fixer system added in 1.13 doesn't support mod data fixers

--- src/main/java/choonster/testmod3/datafix/package-info.java
+++
@@ -1,7 +0,0 @@
-@ParametersAreNonnullByDefault
-@MethodsReturnNonnullByDefault
-package choonster.testmod3.datafix;
-
-import mcp.MethodsReturnNonnullByDefault;
-
-import javax.annotation.ParametersAreNonnullByDefault;
--- src/main/java/choonster/testmod3/init/ModDataFixers.java
+++ src/main/java/choonster/testmod3/init/ModDataFixers.java
@@ -1,31 +1,20 @@
 package choonster.testmod3.init;
 
-import choonster.testmod3.TestMod3;
-import choonster.testmod3.datafix.BlockFlatteningDefinitions;
-import choonster.testmod3.datafix.ItemFlatteningDefinitions;
-import net.minecraft.util.datafix.FixTypes;
-import net.minecraftforge.common.util.ModFixs;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.common.FMLCommonHandler;
-import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-
 /**
  * Registers this mod's data fixers.
  *
  * @author Choonster
  */
-@Mod.EventBusSubscriber(modid = TestMod3.MODID, bus = Bus.MOD)
+// TODO: Update if/when mod data fixers are re-implemented.
+//@Mod.EventBusSubscriber(modid = TestMod3.MODID, bus = Bus.MOD)
 public class ModDataFixers {
-	/**
-	 * The current data version.
-	 */
+ 	/**
+ 	* The current data version.
+ 	*//*
 	public static final int DATA_VERSION = 103;
-
-	/**
+*//**
 	 * Register this mod's data fixers.
-	 */
+ 	*//*
 	@SubscribeEvent
 	public static void registerDataFixers(final FMLCommonSetupEvent event) {
 		final ModFixs modFixs = FMLCommonHandler.instance().getDataFixer().init(TestMod3.MODID, DATA_VERSION);
@@ -33,4 +22,5 @@ public class ModDataFixers {
 		modFixs.registerFix(FixTypes.CHUNK, BlockFlatteningDefinitions.createBlockFlattening());
 		modFixs.registerFix(FixTypes.ITEM_INSTANCE, ItemFlatteningDefinitions.createItemFlattening());
 	}
+*/
 }
