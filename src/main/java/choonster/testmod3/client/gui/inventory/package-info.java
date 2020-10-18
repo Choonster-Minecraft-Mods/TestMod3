28b2df0dac36dd51e57b53094d6a5f95aab47049
Choonster TheMage <Choonster.2010@gmail.com>
2020-10-18 16:46:53 +1100

    Add package-info.java file for choonster.testmod3.client.gui.inventory package

---
+++ src/main/java/choonster/testmod3/client/gui/inventory/package-info.java
@@ -0,0 +1,7 @@
+@ParametersAreNonnullByDefault
+@MethodsReturnNonnullByDefault
+package choonster.testmod3.client.gui.inventory;
+
+import mcp.MethodsReturnNonnullByDefault;
+
+import javax.annotation.ParametersAreNonnullByDefault;
