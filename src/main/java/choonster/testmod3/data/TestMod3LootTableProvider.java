307aebc4c17ad9636f2a3ebb7cb38eb715c329c9
Choonster TheMage <Choonster.2010@gmail.com>
2020-10-18 16:51:21 +1100

    Only validate mod loot tables in loot table provider

--- src/main/java/choonster/testmod3/data/TestMod3LootTableProvider.java
+++ src/main/java/choonster/testmod3/data/TestMod3LootTableProvider.java
@@ -1,21 +1,24 @@
 package choonster.testmod3.data;
 
+import choonster.testmod3.TestMod3;
 import choonster.testmod3.data.loot.TestMod3BlockLootTables;
 import choonster.testmod3.data.loot.TestMod3EntityLootTables;
 import choonster.testmod3.data.loot.TestMod3GenericLootTables;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Sets;
 import com.mojang.datafixers.util.Pair;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.LootTableProvider;
-import net.minecraft.loot.LootParameterSet;
-import net.minecraft.loot.LootParameterSets;
-import net.minecraft.loot.LootTable;
+import net.minecraft.loot.*;
 import net.minecraft.util.ResourceLocation;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 
 /**
  * Generates this mod's loot tables.
@@ -36,6 +39,23 @@ public class TestMod3LootTableProvider extends LootTableProvider {
 	@Override
 	protected List<Pair<Supplier<Consumer<BiConsumer<ResourceLocation, LootTable.Builder>>>, LootParameterSet>> getTables() {
 		return lootTableGenerators;
+	}
+
+	@Override
+	protected void validate(final Map<ResourceLocation, LootTable> map, final ValidationTracker validationtracker) {
+		final Set<ResourceLocation> modLootTableIds = LootTables
+				.getReadOnlyLootTables()
+				.stream()
+				.filter(lootTable -> lootTable.getNamespace().equals(TestMod3.MODID))
+				.collect(Collectors.toSet());
+
+		for (final ResourceLocation id : Sets.difference(modLootTableIds, map.keySet())) {
+			validationtracker.addProblem("Missing mod loot table: " + id);
+		}
+
+		map.forEach((id, lootTable) -> {
+			LootTableManager.validateLootTable(validationtracker, id, lootTable);
+		});
 	}
 
 	/**
