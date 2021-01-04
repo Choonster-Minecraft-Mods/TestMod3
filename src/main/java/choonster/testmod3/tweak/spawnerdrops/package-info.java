4350e479cf98f4f459818593e17e2d0c5656f901
Choonster TheMage <Choonster.2010@gmail.com>
2021-01-05 04:35:15 +1100

    Remove leftover Spawner Drops package-info.java file
    -- The actual logic was moved to a Global Loot Modifier in commit 80ff8cc

--- src/main/java/choonster/testmod3/tweak/spawnerdrops/package-info.java
+++
@@ -1,7 +0,0 @@
-@ParametersAreNonnullByDefault
-@MethodsReturnNonnullByDefault
-package choonster.testmod3.tweak.spawnerdrops;
-
-import mcp.MethodsReturnNonnullByDefault;
-
-import javax.annotation.ParametersAreNonnullByDefault;
