31c94c3722b2ee3ad2f21be291de508aaa4d08f5
Choonster TheMage <Choonster.2010@gmail.com>
2021-01-18 20:44:24 +1100

    Add WIP flow/displacement code for Portal Displacement Fluid

--- src/main/java/choonster/testmod3/fluid/PortalDisplacementFluid.java
+++ src/main/java/choonster/testmod3/fluid/PortalDisplacementFluid.java
@@ -1,14 +1,20 @@
 package choonster.testmod3.fluid;
 
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
+import net.minecraft.block.*;
+import net.minecraft.block.material.Material;
+import net.minecraft.fluid.FlowingFluid;
 import net.minecraft.fluid.Fluid;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.state.StateContainer;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.util.Direction;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.IBlockReader;
 import net.minecraftforge.fluids.ForgeFlowingFluid;
+import net.minecraftforge.fml.common.ObfuscationReflectionHelper;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 
 /**
  * A fluid that displaces portals.
@@ -20,8 +26,21 @@ import net.minecraftforge.fluids.ForgeFlowingFluid;
  */
 // TODO: Properly implement portal displacement
 public abstract class PortalDisplacementFluid extends ForgeFlowingFluid {
+	private static final Method DOES_SIDE_HAVE_HOLES = ObfuscationReflectionHelper.findMethod(FlowingFluid.class, /* doesSideHaveHoles */ "func_212751_a", Direction.class, IBlockReader.class, BlockPos.class, BlockState.class, BlockPos.class, BlockState.class);
+
 	protected PortalDisplacementFluid(final Properties properties) {
 		super(properties);
+	}
+
+	@Override
+	protected boolean canFlow(final IBlockReader worldIn, final BlockPos fromPos, final BlockState fromBlockState, final Direction direction, final BlockPos toPos, final BlockState toBlockState, final FluidState toFluidState, final Fluid fluidIn) {
+		try {
+			return toFluidState.canDisplace(worldIn, toPos, fluidIn, direction) &&
+					(boolean) DOES_SIDE_HAVE_HOLES.invoke(this, direction, worldIn, fromPos, fromBlockState, toPos, toBlockState) &&
+					isNotBlocked(worldIn, toPos, toBlockState, fluidIn);
+		} catch (final IllegalAccessException | InvocationTargetException e) {
+			throw new RuntimeException("Failed to invoke FlowingFluid.doesSideHaveHoles", e);
+		}
 	}
 
 	@Override
@@ -33,6 +52,26 @@ public abstract class PortalDisplacementFluid extends ForgeFlowingFluid {
 		}
 
 		return super.canDisplace(state, world, pos, fluidIn, direction);
+	}
+
+	private boolean isNotBlocked(final IBlockReader world, final BlockPos pos, final BlockState state, final Fluid fluid) {
+		final Block block = state.getBlock();
+
+		if (block instanceof ILiquidContainer) {
+			return ((ILiquidContainer) block).canContainFluid(world, pos, state, fluid);
+		}
+
+		if (!(block instanceof DoorBlock) && !block.isIn(BlockTags.SIGNS) && block != Blocks.LADDER && block != Blocks.SUGAR_CANE && block != Blocks.BUBBLE_COLUMN) {
+			final Material material = state.getMaterial();
+
+			if (material != Material.STRUCTURE_VOID && material != Material.OCEAN_PLANT && material != Material.SEA_GRASS) {
+				return !material.blocksMovement();
+			}
+
+			return false;
+		}
+
+		return false;
 	}
 
 	public static class Flowing extends PortalDisplacementFluid {
