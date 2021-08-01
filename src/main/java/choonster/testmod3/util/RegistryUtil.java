fd591891a8a50ebe7b191273632684e2afd00774
Choonster TheMage <Choonster.2010@gmail.com>
2021-08-02 01:45:56 +1000

    Remove unused RegistryUtil.getRegistryEntry methods

--- src/main/java/choonster/testmod3/util/RegistryUtil.java
+++ src/main/java/choonster/testmod3/util/RegistryUtil.java
@@ -5,7 +5,6 @@ import com.google.common.base.Preconditions;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraftforge.registries.IForgeRegistry;
 import net.minecraftforge.registries.IForgeRegistryEntry;
-import net.minecraftforge.registries.RegistryManager;
 
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -18,35 +17,6 @@ import java.util.stream.StreamSupport;
  * @author Choonster
  */
 public class RegistryUtil {
-	/**
-	 * Get an entry from the provided registry, using <code>testmod3</code> as the mod ID.
-	 *
-	 * @param registry The registry
-	 * @param name     The name of the entry
-	 * @param <T>      The registry type
-	 * @return The registry entry
-	 * @throws NullPointerException When the entry doesn't exist
-	 */
-	public static <T extends IForgeRegistryEntry<T>> T getRegistryEntry(final IForgeRegistry<T> registry, final String name) {
-		return getRegistryEntry(registry, TestMod3.MODID, name);
-	}
-
-	/**
-	 * Get an entry from the provided registry.
-	 *
-	 * @param registry The registry
-	 * @param modid    The mod ID of the entry
-	 * @param name     The name of the entry
-	 * @param <T>      The registry type
-	 * @return The registry entry
-	 * @throws NullPointerException When the entry doesn't exist
-	 */
-	public static <T extends IForgeRegistryEntry<T>> T getRegistryEntry(final IForgeRegistry<T> registry, final String modid, final String name) {
-		final ResourceLocation key = new ResourceLocation(modid, name);
-		final T registryEntry = registry.getValue(key);
-		return Preconditions.checkNotNull(registryEntry, "%s doesn't exist in registry %s", key, RegistryManager.ACTIVE.getName(registry));
-	}
-
 	/**
 	 * Get all of this mod's registry entries from the provided registry.
 	 *
