ddb8b3d4e7333570dd442e32db5767345c4bbb10
Choonster TheMage <Choonster.2010@gmail.com>
2022-03-19 12:28:13 +1100

    Update WAILA (Jade) compat to new sided registration

--- src/main/java/choonster/testmod3/compat/waila/WailaCompat.java
+++ src/main/java/choonster/testmod3/compat/waila/WailaCompat.java
@@ -2,7 +2,7 @@ package choonster.testmod3.compat.waila;
 
 import choonster.testmod3.text.TestMod3Lang;
 import choonster.testmod3.world.level.block.*;
-import mcp.mobius.waila.api.IRegistrar;
+import mcp.mobius.waila.api.IWailaClientRegistration;
 import mcp.mobius.waila.api.IWailaPlugin;
 import mcp.mobius.waila.api.TooltipPosition;
 import mcp.mobius.waila.api.WailaPlugin;
@@ -14,18 +14,18 @@ import mcp.mobius.waila.api.WailaPlugin;
  */
 @WailaPlugin
 public class WailaCompat implements IWailaPlugin {
-
+	@SuppressWarnings("UnstableApiUsage")
 	@Override
-	public void register(final IRegistrar registrar) {
-		registrar.registerComponentProvider(new RotatableHUDHandler(ColoredRotatableBlock.FACING), TooltipPosition.BODY, ColoredRotatableBlock.class);
-		registrar.registerComponentProvider(new MultiRotatableHUDHandler(ColoredMultiRotatableBlock.FACE_ROTATION), TooltipPosition.BODY, ColoredMultiRotatableBlock.class);
+	public void registerClient(final IWailaClientRegistration registration) {
+		registration.registerComponentProvider(new RotatableHUDHandler(ColoredRotatableBlock.FACING), TooltipPosition.BODY, ColoredRotatableBlock.class);
+		registration.registerComponentProvider(new MultiRotatableHUDHandler(ColoredMultiRotatableBlock.FACE_ROTATION), TooltipPosition.BODY, ColoredMultiRotatableBlock.class);
 
-		registrar.registerComponentProvider(new RotatableHUDHandler(RotatableLampBlock.FACING), TooltipPosition.BODY, RotatableLampBlock.class);
-		registrar.registerComponentProvider(new RotatableHUDHandler(ModChestBlock.FACING), TooltipPosition.BODY, ModChestBlock.class);
+		registration.registerComponentProvider(new RotatableHUDHandler(RotatableLampBlock.FACING), TooltipPosition.BODY, RotatableLampBlock.class);
+		registration.registerComponentProvider(new RotatableHUDHandler(ModChestBlock.FACING), TooltipPosition.BODY, ModChestBlock.class);
 
-		registrar.registerComponentProvider(new RotatableHUDHandler(PlaneBlock.HORIZONTAL_ROTATION, TestMod3Lang.BLOCK_DESC_PLANE_HORIZONTAL_ROTATION.getTranslationKey()), TooltipPosition.BODY, PlaneBlock.class);
-		registrar.registerComponentProvider(new VerticalRotatableHUDHandler(PlaneBlock.VERTICAL_ROTATION), TooltipPosition.BODY, PlaneBlock.class);
+		registration.registerComponentProvider(new RotatableHUDHandler(PlaneBlock.HORIZONTAL_ROTATION, TestMod3Lang.BLOCK_DESC_PLANE_HORIZONTAL_ROTATION.getTranslationKey()), TooltipPosition.BODY, PlaneBlock.class);
+		registration.registerComponentProvider(new VerticalRotatableHUDHandler(PlaneBlock.VERTICAL_ROTATION), TooltipPosition.BODY, PlaneBlock.class);
 
-		registrar.registerComponentProvider(new RestrictedFluidTankEnabledFacingsHUDHandler(), TooltipPosition.BODY, RestrictedFluidTankBlock.class);
+		registration.registerComponentProvider(new RestrictedFluidTankEnabledFacingsHUDHandler(), TooltipPosition.BODY, RestrictedFluidTankBlock.class);
 	}
 }
