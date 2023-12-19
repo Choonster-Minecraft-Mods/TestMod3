1caf7bdea16ebab117c8a8f49ea1081558a683b2
Choonster TheMage <Choonster.2010@gmail.com>
2023-12-20 04:04:01 +1100

    Add missing ticker for PotionEffectBlock

--- src/main/java/choonster/testmod3/world/level/block/PotionEffectBlock.java
+++ src/main/java/choonster/testmod3/world/level/block/PotionEffectBlock.java
@@ -1,10 +1,14 @@
 package choonster.testmod3.world.level.block;
 
+import choonster.testmod3.init.ModBlockEntities;
 import choonster.testmod3.world.level.block.entity.PotionEffectBlockEntity;
 import com.mojang.serialization.MapCodec;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.entity.BlockEntity;
+import net.minecraft.world.level.block.entity.BlockEntityTicker;
+import net.minecraft.world.level.block.entity.BlockEntityType;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.Nullable;
 
@@ -32,5 +36,11 @@ public class PotionEffectBlock extends BaseEntityBlock<PotionEffectBlockEntity> 
 	@Override
 	public BlockEntity newBlockEntity(final BlockPos pos, final BlockState state) {
 		return new PotionEffectBlockEntity(pos, state);
+	}
+
+	@Nullable
+	@Override
+	public <T extends BlockEntity> BlockEntityTicker<T> getTicker(final Level level, final BlockState state, final BlockEntityType<T> blockEntityType) {
+		return createTickerHelper(blockEntityType, ModBlockEntities.POTION_EFFECT.get(), PotionEffectBlockEntity::tick);
 	}
 }
