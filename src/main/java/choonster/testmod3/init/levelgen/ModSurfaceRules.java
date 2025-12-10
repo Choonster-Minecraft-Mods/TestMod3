c1784f8ae0731caa815aa2196fa4adabf49c0c0d
Choonster TheMage <Choonster.2010@gmail.com>
2025-12-10 19:54:05 +1100

    Comment out remaining Terrablender references

--- src/main/java/choonster/testmod3/init/levelgen/ModSurfaceRules.java
+++ src/main/java/choonster/testmod3/init/levelgen/ModSurfaceRules.java
@@ -1,13 +1,11 @@
 package choonster.testmod3.init.levelgen;
 
 import choonster.testmod3.TestMod3;
-import choonster.testmod3.world.level.levelgen.surfacerule.TestMod3OverworldSurfaceRuleData;
 import net.minecraft.world.level.levelgen.SurfaceRules;
 import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber.Bus;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
-import terrablender.api.SurfaceRuleManager;
 
 /**
  * Registers this mod's {@link SurfaceRules.RuleSource SurfaceRule Sources} with TerraBlender.
@@ -19,7 +17,7 @@ public class ModSurfaceRules {
 	@SubscribeEvent
 	public static void commonSetup(final FMLCommonSetupEvent event) {
 		event.enqueueWork(() -> {
-			SurfaceRuleManager.addSurfaceRules(SurfaceRuleManager.RuleCategory.OVERWORLD, TestMod3.MODID, TestMod3OverworldSurfaceRuleData.makeRules());
+//			SurfaceRuleManager.addSurfaceRules(SurfaceRuleManager.RuleCategory.OVERWORLD, TestMod3.MODID, TestMod3OverworldSurfaceRuleData.makeRules());
 		});
 	}
 }
